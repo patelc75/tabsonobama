9faabcb2ac3f9554473856b6fb0624683319b494
Brent Hargrave <brent.hargrave@gmail.com>
2009-01-12 13:04:33 -0800

    seed affiliation table [#48 tagged:committed responsible:jamescallmebrent]

--- db/migrate/20090112202216_create_affiliations.rb
+++ db/migrate/20090112202216_create_affiliations.rb
@@ -2,11 +2,12 @@ class CreateAffiliations < ActiveRecord::Migration
   def self.up
     create_table :affiliations do |t|
       t.string :name
-
       t.timestamps
     end
+    
+    %w{ Democrat Republican Independent }.each { |a| Affiliation.create(:name => a) }
   end
-
+  
   def self.down
     drop_table :affiliations
   end
