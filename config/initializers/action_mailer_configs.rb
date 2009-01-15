4125984083a73644739a4c155323ab1438d2618b
Brent Hargrave <brent.hargrave@gmail.com>
2009-01-15 08:03:21 -0800

    fork ActionMailer configs by RAILS_ENV [#43 tagged:committed responsible:jamescallmebrent]

--- config/initializers/action_mailer_configs.rb
+++ config/initializers/action_mailer_configs.rb
@@ -1,5 +1,13 @@
 require "smtp_tls"
 
-mailer_config = File.open("#{RAILS_ROOT}/config/gmail.yml")
-mailer_options = YAML.load(mailer_config)
-ActionMailer::Base.smtp_settings = mailer_options
+if (ENV['RAILS_ENV'] == 'production')
+  ActionMailer::Base.smtp_settings = {
+    :address => "localhost" ,
+    :port => 25,
+    :domain => "tabsonobama.org"
+  }
+else
+  mailer_config = File.open("#{RAILS_ROOT}/config/gmail.yml")
+  mailer_options = YAML.load(mailer_config)
+  ActionMailer::Base.smtp_settings = mailer_options
+end
