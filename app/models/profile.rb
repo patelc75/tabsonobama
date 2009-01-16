1a9cdf5b1caa21bc12da67e62d5c59129f756c6b
Brent Hargrave <brent.hargrave@gmail.com>
2009-01-16 08:38:29 -0800

    added Profile spec; validating first/last name [#36 tagged:committed responsible:jamescallmebrent]

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,9 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
-
-  validates_format_of :name, :with => Authentication.name_regex, :message => Authentication.bad_name_message, :allow_nil => true
-  validates_length_of :name, :maximum => 100
+  
+  validates_format_of :first_name, :with => Authentication.name_regex, :message => Authentication.bad_name_message, :allow_nil => true
+  validates_length_of :first_name, :maximum => 50
+  
+  validates_format_of :last_name, :with => Authentication.name_regex, :message => Authentication.bad_name_message, :allow_nil => true
+  validates_length_of :last_name, :maximum => 50
 end
---
+++ spec/models/profile_spec.rb
@@ -0,0 +1,73 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Profile do
+  
+  describe 'allows legitimate first names:' do
+    ['Andre The Giant (7\'4", 520 lb.) -- has a posse',
+     '', '1234567890_234567890_234567890_234567890_234567890',
+    ].each do |name_str|
+      it "'#{name_str}'" do
+        lambda do
+          profile = create_profile(:first_name => name_str)
+          profile.errors.on(:first_name).should be_nil
+        end.should change(Profile, :count).by(1)
+      end
+    end
+  end
+  
+  describe "disallows illegitimate first names" do
+    ["tab\t", "newline\n",
+     '1234567890_234567890_234567890_234567890_234567890_',
+     ].each do |name_str|
+      it "'#{name_str}'" do
+        lambda do
+          profile = create_profile(:first_name => name_str)
+          profile.errors.on(:first_name).should_not be_nil
+        end.should_not change(Profile, :count)
+      end
+    end
+  end
+  
+  describe 'allows legitimate last names:' do
+    ['Andre The Giant (7\'4", 520 lb.) -- has a posse',
+     '', '1234567890_234567890_234567890_234567890_234567890',
+    ].each do |name_str|
+      it "'#{name_str}'" do
+        lambda do
+          profile = create_profile(:last_name => name_str)
+          profile.errors.on(:last_name).should be_nil
+        end.should change(Profile, :count).by(1)
+      end
+    end
+  end
+
+  describe "disallows illegitimate first names" do
+    ["tab\t", "newline\n",
+     '1234567890_234567890_234567890_234567890_234567890_',
+     ].each do |name_str|
+      it "'#{name_str}'" do
+        lambda do
+          profile = create_profile(:last_name => name_str)
+          profile.errors.on(:last_name).should_not be_nil
+        end.should_not change(Profile, :count)
+      end
+    end
+  end
+  
+  protected
+    def create_user(options = {})
+      record = User.new({ :login => 'quire', :email => 'quire@example.com', :password => 'quire69', :password_confirmation => 'quire69' }.merge(options))
+      record.register! if record.valid?
+      record
+    end
+    
+    def create_profile(options = {})
+      attrs = {
+        :first_name => 'quire',
+        :last_name => 'quire',
+        :location => 'Brooklyn, NY',
+        :user => create_user
+      }.merge(options)
+      Profile.create(attrs)
+    end
+end
