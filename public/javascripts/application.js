240508019396171a7b5cc693d25a46918fe1f978
Cory <cory.forsyth@gmail.com>
2009-01-18 15:58:35 -0500

    Separated JS $(document).ready calls to application.js.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,41 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+/* setup event handlers for the links in the login/signup nav */
+function setupLognNav() {
+  $("#login-link").click(function(event) {
+    toggleLoginForm();
+    event.preventDefault();
+  });
+  
+  $("#cancel-button").click(function(event) {
+    toggleLoginForm();
+    event.preventDefault();
+  });
+  
+  $("#login-again-link").click(function(event) {
+    $("#login-form").show();
+    $("#login-links").hide();
+    $("#login-again").hide();
+    event.preventDefault();
+  });
+}
+
+function showLoginAgain() {
+  $("#login-again").show();
+  $("#login-links").hide();
+  $("#login-form").hide();
+}
+
+function toggleLoginForm() {
+  $("#login-links").toggle();
+  $("#login-form").toggle();
+}
+
+/* setup event handler for the flash notice div */
+function setupNotice() {
+  $("#close-notice").click(function () {
+    $("#notice").fadeOut("slow");
+  });
+}
+
