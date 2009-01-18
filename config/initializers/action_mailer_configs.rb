af197c3ea891455fa82b2ff5c73c4a306274c989
Brent Hargrave <brent.hargrave@gmail.com>
2009-01-18 12:31:56 -0800

    ActionMailer now uses localhost for production/staging, gmail for dev [#61 tagged:committed responsible:jamescallmebrent]

--- config/initializers/action_mailer_configs.rb
+++ config/initializers/action_mailer_configs.rb
@@ -1,13 +1,19 @@
 require "smtp_tls"
 
-if (ENV['RAILS_ENV'] == 'production')
-  ActionMailer::Base.smtp_settings = {
-    :address => "localhost" ,
-    :port => 25,
-    :domain => "tabsonobama.org"
-  }
+gmail_configs = File.open("#{RAILS_ROOT}/config/gmail.yml")
+SMTP_SETTINGS_GMAIL = YAML.load(gmail_configs)
+
+SMTP_SETTINGS_LOCALHOST = {
+  :address => "localhost" ,
+  :port => 25,
+  :domain => "tabsonobama.org"
+}
+
+case ENV['RAILS_ENV']
+when 'production'
+  ActionMailer::Base.smtp_settings = SMTP_SETTINGS_LOCALHOST
+when 'staging'
+  ActionMailer::Base.smtp_settings = SMTP_SETTINGS_LOCALHOST
 else
-  mailer_config = File.open("#{RAILS_ROOT}/config/gmail.yml")
-  mailer_options = YAML.load(mailer_config)
-  ActionMailer::Base.smtp_settings = mailer_options
+  ActionMailer::Base.smtp_settings = SMTP_SETTINGS_GMAIL
 end
