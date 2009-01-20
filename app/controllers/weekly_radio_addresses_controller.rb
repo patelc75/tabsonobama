beb0eb68352fee5707a78e4a5654164793a55db2
Brent Hargrave <brent.hargrave@gmail.com>
2009-01-19 18:01:43 -0800

    added/updated index and show views for weekly addresses and cabinet members [#63 responsible:jamescallmebrent tagged:committed]

--- app/controllers/weekly_radio_addresses_controller.rb
+++ app/controllers/weekly_radio_addresses_controller.rb
@@ -1,2 +1,11 @@
 class WeeklyRadioAddressesController < ApplicationController
+  
+  def index
+    @addresses = WeeklyRadioAddress.find(:all)
+  end
+  
+  def show
+    @address = WeeklyRadioAddress.find(params[:id])
+  end
+  
 end
