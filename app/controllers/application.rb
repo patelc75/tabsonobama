823e869a15dab2ecac94749f90dcee6e0e403ed8
Brent Hargrave <brent.hargrave@gmail.com>
2009-01-25 19:12:06 -0800

    doh! pop_signup_form loaded from header, so must initialize user/profile variables through a global before_filter in application.rb [#97 responsible:jamescallmebrent tagged:committed]

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -9,12 +9,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery :secret => 'b0a876313f3f9195e9bd01473bc5cd06'
   filter_parameter_logging :password, :password_confirmation
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
+  before_filter :build_user_and_profile
   
   protected
   
   # Automatically respond with 404 for ActiveRecord::RecordNotFound
   def record_not_found
     render :file => File.join(RAILS_ROOT, 'public', '404.html'), :status => 404
+  end
+  
+  def build_user_and_profile
+    @user = User.new
+    @profile = @user.build_profile
   end
 end
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
   
   def index
-    @user = User.new
-    @profile = @user.build_profile
   end
   
 end
