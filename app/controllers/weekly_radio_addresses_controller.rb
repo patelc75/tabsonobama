91ef8f580ee2c9a611ae190473f8c4cd447db4fa
Chirag Patel <patelc75@yahoo.com>
2009-03-10 07:10:29 -0500

    weekly radio short list, add promotion links, etc

--- app/controllers/weekly_radio_addresses_controller.rb
+++ app/controllers/weekly_radio_addresses_controller.rb
@@ -8,4 +8,25 @@ class WeeklyRadioAddressesController < ApplicationController
     @address = WeeklyRadioAddress.find_by_permalink(params[:id])
   end
   
+  def new
+    @weekly = WeeklyRadioAddress.new
+  end
+
+  def create
+    @weekly = WeeklyRadioAddress.new(params[:weekly_radio_address])
+    if @weekly.save
+      flash[:notice] = 'Weekly Radio Address created.'
+      redirect_to("/weekly-radio-addresses/#{@weekly.permalink}")
+    else
+      render :action => "new"
+    end
+  end
+  
+  def delete
+  	@address = WeeklyRadioAddress.find_by_permalink(params[:id])
+  	if @address.delete
+  	  flash[:notice] = 'Weekly Radio Address deleted.'
+      redirect_to :controller => "weekly-radio-addresses", :action => "index"	
+  	end
+  end
 end
