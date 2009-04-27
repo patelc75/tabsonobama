8628acd9c49b86534e1e4d8b3053ce451e4ab32a
Chirag Patel <patelc75@yahoo.com>
2009-04-27 00:10:04 -0400

    update settings.yml to launch private beta on www(production)

--- app/models/weekly_radio_address.rb
+++ app/models/weekly_radio_address.rb
@@ -4,9 +4,10 @@ class WeeklyRadioAddress < ActiveRecord::Base
   include Promotable
   has_one :promotion, :as => :item, :dependent => :destroy
   
-  has_permalink :timestamp
+  has_permalink :timestamp #, :to_param => self.permalink[0..10]
+  
   def to_param
-    self.permalink[0..10]
+    self.permalink #[0..10]
   end
   
   def self.featured
