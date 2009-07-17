1f9a65f25cb1075180e542e73527064c02a9d3e6
Sandeep Naidu <admin@leadersahead.com>
2009-07-17 02:16:54 -0400

    Automatically goingto the home page after Activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,11 +60,11 @@ class UsersController < ApplicationController
       #flash[:notice] = "Signup complete! Please sign in to continue."
       respond_to do |format|
         #format.html { redirect_to login_path }
-        #format.html { redirect_to "http://staging.tabsonobama.org/home"}
+        format.html { redirect_to "http://staging.tabsonobama.org/home"}
         #format.html {redirect_to : action=> "/home"}
-       # format.html { render :controller => :users, :action => :success_activate }
-        #format.js { render :partial => "/users/pop_signup_form" }
-        format.js { render :partial => "/users/nav_login_form" }
+        format.html { render :controller => :users, :action => :success_activate }
+        format.js { render :partial => "/users/pop_signup_form" }
+        #format.js { render :partial => "/users/nav_login_form" }
       end
     when params[:activation_code].blank?
       flash[:error] = "The activation code was missing.  Please follow the URL from your email."
