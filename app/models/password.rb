7bd3faee4de046b3f9b71d23ae635dbf5a5eedf1
Chirag Patel <chirag@tabsonobama.org>
2009-08-31 00:38:24 -0400

    remove validation for the user column for forgotten password [#192 responsible:patelc75]

--- app/models/password.rb
+++ app/models/password.rb
@@ -5,7 +5,7 @@ class Password < ActiveRecord::Base
   
   belongs_to :user
   
-  validates_presence_of :email, :user
+  validates_presence_of :email#, :user
   validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => 'is not a valid email address'
 
   protected  
